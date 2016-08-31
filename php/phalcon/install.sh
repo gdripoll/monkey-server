660b91622e48c1922b31ad21234789c1b420e1fc
gdripoll <gdripoll@gmail.com>
2016-08-31 14:43:19 -0300

    + Phalcon

---
+++ php/phalcon/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+echo -e "==========\nWORKING\n=========="
+cd /tmp
+curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
+apt-get update
+apt-get -y install php7.0-phalcon
+
+echo -e "==========\nREADY\n=========="
+
