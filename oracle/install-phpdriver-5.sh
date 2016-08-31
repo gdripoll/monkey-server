bc26a2dbb4c61f453559493882ceca78c345ea0f
gdripoll <gdripoll@gmail.com>
2016-08-31 17:13:12 -0300

    Oci8 bug fixed

--- oracle/install-phpdriver-5.sh
+++ oracle/install-phpdriver-5.sh
@@ -2,21 +2,16 @@
 clear
 echo -e "==========\nWORKING\n=========="
 
-cp ./soft/* /tmp && \
-cd /tmp && \
+mkdir /tmp/oracle &&\
+cp ./soft/* /tmp/oracle && \
+cd /tmp/oracle && \
 unzip instantclient-basic-linux.x64-12.1.0.2.0.zip && \
 unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip && \
 mkdir -p /opt/oracle/ && \
 mv instantclient_12_1 /opt/oracle/instantclient && \
 ln -sf /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so && \
-wget -q https://pecl.php.net/get/oci8-2.0.10.tgz && \
-tar -xzvf oci8-2.0.10.tgz && \
-cd oci8-2.0.10 && \
-phpize && \
-./configure --with-oci8=shared,instantclient,/opt/oracle/instantclient && \
-make && \
-make install && \
-rm -rf /tmp/*
+
+pecl install oci8-2.0.12
 
 echo -e "==========\nREADY\n=========="
 
---
+++ oracle/install-phpdriver-7.0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo -e "==========\nWORKING\n=========="
+
+mkdir /tmp/oracle &&\
+cp ./soft/* /tmp/oracle && \
+cd /tmp/oracle && \
+unzip instantclient-basic-linux.x64-12.1.0.2.0.zip && \
+unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip && \
+mkdir -p /opt/oracle/ && \
+mv instantclient_12_1 /opt/oracle/instantclient && \
+ln -sf /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so && \
+
+pecl install oci8-1.4.10
+
+echo -e "==========\nREADY\n=========="
+
