e670710b700e8ed696bf00a4201b3cc735bf6c0f
Gustavo Ripoll <gdripoll@gmail.com>
2016-09-02 13:29:39 -0300

    fix upgrade

--- system/upgrade.sh
+++ system/upgrade.sh
@@ -3,6 +3,6 @@ clear
 echo -e "==========\nWORKING\n=========="
 apt-get update
 apt-get full-upgrade -y
-apt-get autoremove
+apt-get autoremove -y
 apt-get clean
 echo -e "==========\nREADY\n=========="
