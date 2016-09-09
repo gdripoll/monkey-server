e532020939fbb08f61db84bd47e061858afbc921
root <root@server>
2016-09-09 15:09:56 -0300

    +phpunit

---
+++ php/phpunit/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo -e "==========\nWORKING\n=========="
+cd /opt
+mkdir phpunit
+chmod -R 777 phpunit
+cd phpunit
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+ln -s /opt/phpunit/phpunit.phar /usr/bin/phpunit
+cd /
+phpunit --version
+
+echo -e "==========\nREADY\n=========="
+
