6a1514fe6bcc1a97df273af896cd6e2f6e544b0c
gdripoll <gdripoll@gmail.com>
2016-09-29 13:43:15 -0300

    *fix utilities

--- system/utils.sh
+++ system/utils.sh
@@ -11,17 +11,20 @@ apt-get -y install  \
 apt-get autoremove
 apt-get clean
 
-echo -e "==========\nSETTING\n=========="
-su $USERNAME
-echo "alias ll='ls -al'" >> ~/.bashrc
-echo "set tabstop=2" > ~/.vimrc
-echo "set nu" >> ~/.vimrc
-echo "syntax on" >> ~/.vimrc
-exit
+echo -e "==========\nSETTING $USERNAME \n=========="
 
-echo "alias ll='ls -al'" >> ~/.bashrc
-echo "set tabstop=2" > ~/.vimrc
-echo "set nu" >> ~/.vimrc
-echo "syntax on" >> ~/.vimrc
+echo "alias ll='ls -al'" >> /home/$USERNAME/.bashrc
+echo "set tabstop=2" > /home/$USERNAME/.vimrc
+echo "set nu" >> /home/$USERNAME/.vimrc
+echo "syntax on" >> /home/$USERNAME/.vimrc
+echo "set nowrap" >> /home/$USERNAME/.vimrc
+
+echo -e "==========\nSETTING root \n=========="
+
+echo "alias ll='ls -al'" >> /root/.bashrc
+echo "set tabstop=2" > /root/.vimrc
+echo "set nu" >> /root/.vimrc
+echo "syntax on" >> /root/.vimrc
+echo "set nowrap" >> /root/.vimrc
 
 echo -e "==========\nREADY\n=========="
