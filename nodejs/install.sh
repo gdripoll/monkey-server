f6b51eb26984eea00dc351caad03c0f5686916b9
gdripoll <gdripoll@gmail.com>
2016-09-29 13:30:16 -0300

    +nodejs

---
+++ nodejs/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+echo -e "==========\nWORKING\n=========="
+
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo -e "==========\nREADY\n=========="
+
