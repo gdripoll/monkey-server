6333d85e89d52c222e8a03aac73756ab2894c6d2
gdripoll <gdripoll@gmail.com>
2017-01-11 13:44:40 -0300

    +fix nginx-old

--- nginx/nginx-old/install.sh
+++ nginx/nginx-old/install.sh
@@ -4,16 +4,10 @@ echo -e "==========\nWORKING\n=========="
 apt-get install -y  \
   nginx
 
-mkdir -p /var/www/local/public
-echo -e "<html>\n\t<h1>dev.local</h1>\n</html>" > /var/www/local/public/index.html
-mkdir -p /var/www/sandbox
-echo -e "<?php\nphpinfo();\n" > /var/www/sandbox/index.php
-
-
 useradd -s /bin/false nginx
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.ori
-cp /home/user/build/config/nginx/nginx.conf /etc/nginx/nginx.conf
+cp ./config/nginx.conf /etc/nginx/nginx.conf
 rm -fr /etc/nginx/conf.d/*
-cp /home/user/build/config/nginx/conf.d/* /etc/nginx/conf.d/
-cp -r /home/user/build/soft/inetpub /
+cp ./config/conf.d/* /etc/nginx/conf.d/
+mkdir /inetpub
 chmod -R 777 /inetpub/
