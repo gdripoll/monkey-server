8221b15e3a4519b7777acb0308a62ef913d8b1ff
gdripoll <gdripoll@gmail.com>
2017-01-20 18:35:31 -0300

    fix phalcon

--- php/phalcon/install.sh
+++ php/phalcon/install.sh
@@ -3,7 +3,7 @@
 clear
 echo -e "==========\nWORKING\n=========="
 cd /tmp
-curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | bash
 apt-get update
 apt-get -y install php7.0-phalcon
 
