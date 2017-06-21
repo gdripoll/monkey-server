a492230a6500834ae17af35f6b77d144e0f9a557
gdripoll <gdripoll@gmail.com>
2017-06-21 11:41:58 -0300

    +node8

---
+++ nodejs/install.v8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+echo -e "==========\nWORKING\n=========="
+
+
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
+
+echo -e "==========\nREADY\n=========="
+
