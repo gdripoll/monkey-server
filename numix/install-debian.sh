55f49bf64e9ca0bc41c357ed4be89a8d55b90ec4
gdripoll <gdripoll@gmail.com>
2017-08-14 11:23:57 -0300

    +numix

---
+++ numix/install-debian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0F164EEB
+
+# nano /etc/apt/sources.list.d/numix.list
+echo "deb http://ppa.launchpad.net/numix/ppa/ubuntu trusty main"      > /etc/apt/sources.list.d/numix.list
+echo "deb-src http://ppa.launchpad.net/numix/ppa/ubuntu trusty main" >> /etx/apt/sources.list.d/numix.list
+
+apt-get update
+apt-get install gtk2-engines-murrine
+apt-get install numix-gtk-theme numix-icon-theme numix-icon-theme-circle numix-icon-theme-bevel
+
+
---
+++ numix/install-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+
+add-apt-repository sudo ppa:numix/ppa
+apt-get update
+apt-get install gtk2-engines-murrine
+apt-get install numix-gtk-theme numix-icon-theme numix-icon-theme-circle numix-icon-theme-bevel
+
+
