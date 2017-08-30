7cf53910319d8241ccefd42b7636d4d4a51471e8
gdripoll <gdripoll@gmail.com>
2017-08-30 09:54:24 -0300

    +rabbitmq

---
+++ rabbit/install.ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+
+echo -- adding source
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.li
+
+echo -- download signature
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+
+echo -- update
+apt update
+
+echo -- install
+apt install -y rabbitmq-server
+
+echo --- enable plugin
+rabbitmq-plugins enable rabbitmq_management
+
+
