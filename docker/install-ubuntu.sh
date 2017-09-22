cbba45b76607535d94c5d011542c809854df2f04
gdripoll <gdripoll@gmail.com>
2017-09-22 18:37:37 -0300

    *fix docker

--- docker/install-ubuntu.sh
+++ docker/install-ubuntu.sh
@@ -17,7 +17,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce
+apt-get install docker-ce docker-compose
 docker run hello-world
 
 echo "-----------------------------------------"
