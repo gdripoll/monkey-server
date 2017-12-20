effd906192d67db932e6ffea483d9f29259ee563
gdripoll <gdripoll@gmail.com>
2017-12-20 14:52:26 -0300

    new mongo

--- mongo/install.sh
+++ mongo/install.sh
@@ -2,7 +2,8 @@
 clear
 echo -e "==========\nWORKING\n=========="
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+#echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 apt-get update
 apt-get install -y mongodb-org
 
