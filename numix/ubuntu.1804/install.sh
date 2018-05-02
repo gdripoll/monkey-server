17f43a83a3ba3c835052ab8607e784ff7b229837
gdripoll <gdripoll@gmail.com>
2018-05-02 11:37:42 -0300

    +numix.ub.1804

---
+++ numix/ubuntu.1804/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+
+add-apt-repository ppa:numix/ppa
+apt update
+apt install -y numix-icon-theme numix-folders
+apt install -y numix-icon-theme-circle 
+apt install -y numix-icon-theme-square
+
