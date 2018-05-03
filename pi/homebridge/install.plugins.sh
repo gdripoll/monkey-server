16cb2d87892844af539d26734477b11bcf2ad26a
gdripoll <gdripoll@gmail.com>
2018-05-03 16:49:05 -0300

    pi-homebridge

---
+++ pi/homebridge/install.plugins.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+clear
+
+
+# https://github.com/nfarina/homebridge-dummy
+# -----------------------------------------
+npm install -g homebridge-dummy
+# -----------------------------------------
+
+
+# https://github.com/rudders/homebridge-http
+# -----------------------------------------
+npm install -g homebridge-http
+# -----------------------------------------
+
+
+# https://www.npmjs.com/package/homebridge-http-webhooks
+# -----------------------------------------
+npm install -g homebridge-http-webhooks
+# -----------------------------------------
+
+
+# https://github.com/KraigM/homebridge-nest
+# -----------------------------------------
+#	{
+#		"platform": "Nest",
+#   		
+#		"token" : "token will be generated upon first run of homebridge without a token",
+#   		
+#		"clientId": "developer Product ID",
+#		"clientSecret": "developer Product Secret.",
+#		"code": "your Pincode from Nest",
+#
+#		"username" : "username",
+#		"password" : "password"
+#	}
+# -----------------------------------------
+npm install -g homebridge-nest
+# -----------------------------------------
+
