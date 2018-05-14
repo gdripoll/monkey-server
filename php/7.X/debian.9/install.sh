3ab07d00048b1c9341f363117ec27f1eab6c6af8
gdripoll <gdripoll@gmail.com>
2018-05-14 15:54:51 -0300

    reordenamiento

---
+++ php/7.X/debian.9/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+
+apt udate
+apt install apt-transport-https lsb-release ca-certificates
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo apt update
+
+apt install -y \
+    php7.2 php7.2-common php7.2-cli php7.2-fpm \
+    php7.2-mysql \
+    php7.2-xml \
+    php7.2-curl \
+    php7.2-gd \
+    php7.2-json
+    php7.2-mbstring \
+    php7.2-mcrypt \
+    php7.2-zip \
+    php7.2-bcmath \
+    php7.2-mysql
+
+/etc/init.d/php7.2-fpm restart
+
