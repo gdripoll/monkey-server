939c5c1bc49537a564dbe0168a5f9cd0d20faf22
gdripoll <gdripoll@gmail.com>
2018-05-15 11:24:08 -0300

    oracle php7.2

---
+++ oracle/install-phpdriver-7.2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo -e "==========\nWORKING\n=========="
+apt update
+apt -y install libaio1 libaio-dev
+
+mkdir /tmp/oracle &&\
+cp ./soft/* /tmp/oracle && \
+cd /tmp/oracle && \
+unzip instantclient-basic-linux.x64-12.1.0.2.0.zip && \
+unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip && \
+mkdir -p /opt/oracle/ && \
+mv instantclient_12_1 /opt/oracle/instantclient && \
+ln -sf /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so && \
+
+#pecl install oci8-1.4.10
+pecl channel-update pecl.php.net
+pecl install oci8
+
+echo -e "==========\nREADY\n=========="
+
--- php/7.X/debian.9/install.sh
+++ php/7.X/debian.9/install.sh
@@ -13,12 +13,12 @@ apt install -y \
     php7.2-xml \
     php7.2-curl \
     php7.2-gd \
-    php7.2-json
+    php7.2-json \
     php7.2-mbstring \
-    php7.2-mcrypt \
     php7.2-zip \
     php7.2-bcmath \
-    php7.2-mysql
+    php7.2-mysql \
+    php-pear
 
 /etc/init.d/php7.2-fpm restart
 
