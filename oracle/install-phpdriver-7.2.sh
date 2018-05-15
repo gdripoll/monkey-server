fb67263a1059eb2292d446a9d5d7693cf1dd1ea0
gdripoll <gdripoll@gmail.com>
2018-05-15 12:40:57 -0300

    instantclient

--- oracle/install-phpdriver-7.2.sh
+++ oracle/install-phpdriver-7.2.sh
@@ -7,8 +7,8 @@ apt -y install libaio1 libaio-dev
 mkdir /tmp/oracle &&\
 cp ./soft/* /tmp/oracle && \
 cd /tmp/oracle && \
-unzip instantclient-basic-linux.x64-12.1.0.2.0.zip && \
-unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip && \
+unzip instantclient-basic-linux.x64-12.2.0.1.0 && \
+unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
 mkdir -p /opt/oracle/ && \
 mv instantclient_12_1 /opt/oracle/instantclient && \
 ln -sf /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so && \
