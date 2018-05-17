76fa8820abbae3559ebb9b74d625037264ec4285
gdripoll <gdripoll@gmail.com>
2018-05-17 12:03:20 -0300

    +phpcs

---
+++ php/phpcs/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo -e "==========\nWORKING\n=========="
+
+mkdir /opt/phpcs &&\
+cd /opt/phpcs &&\
+wget https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar &&\
+wget https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar &&\
+chmod uog+x phpcs.phar phpcbf.phar &&\
+ln -s /opt/phpcs/phpcs.phar /usr/local/bin/phpcs
+ls -s /opt/phpcs/phpcbf.phar /usr/local/bin/phpcbf.phar
+
+echo -e "==========\nREADY\n=========="
+
