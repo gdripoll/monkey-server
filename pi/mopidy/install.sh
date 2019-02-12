8c22c4788fa95e612c0e5a5a79eb31d591919d2c
Gustavo D. Ripoll <gdripoll@gmail.com>
2019-02-12 14:40:10 -0300

    keys

---
+++ pi/mopidy/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
+apt update
+apt install mopidy
+
