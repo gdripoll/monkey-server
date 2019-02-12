c396d5f1c43c206fb77ba35991ecbda8ca11a693
gdripoll <gdripoll@gmail.com>
2019-02-12 14:45:37 -0300

    fix+numix

--- numix/install-bl-Helium.sh
+++ numix/install-bl-Helium.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear
 
+add-apt-repository ppa:numix/ppa
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 52B709720F164EEB 
 apt update
-apt install numix-gtk-theme numix-icon-theme
+apt install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
