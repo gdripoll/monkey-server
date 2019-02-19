6418499a488b2c1bba88657cbedb042eb0a571ab
gdripoll <gdripoll@gmail.com>
2019-02-19 16:31:34 -0300

    phpunit6

--- php/phpunit/install.v6.sh
+++ php/phpunit/install.v6.sh
@@ -6,9 +6,10 @@ cd /opt
 mkdir phpunit
 chmod -R 777 phpunit
 cd phpunit
-wget -O phpunit.v7.phar https://phar.phpunit.de/phpunit-7.phar
+wget -O phpunit.v6.phar https://phar.phpunit.de/phpunit-6.phar
 chmod uog+x phpunit
-ln -s /opt/phpunit/phpunit.v7.phar /usr/bin/phpunit
+rm -rf /usr/bin/phpunit
+ln -s /opt/phpunit/phpunit.v6.phar /usr/bin/phpunit
 cd /
 phpunit --version
 
