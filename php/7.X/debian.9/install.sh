590669ec425fa0e9ffa1ccf98c0b5aeffa285f2e
gdripoll <gdripoll@gmail.com>
2019-02-27 12:49:46 -0300

    fix php7.2 bunsen

--- php/7.X/debian.9/install.sh
+++ php/7.X/debian.9/install.sh
@@ -4,7 +4,7 @@ clear
 apt udate
 apt install apt-transport-https lsb-release ca-certificates
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sh -c 'echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list'
 apt update
 
 apt install -y \
