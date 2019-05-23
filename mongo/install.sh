c000d4cbed8862988ed8e6a7f4a21eef06bc2691
gdripoll <gdripoll@gmail.com>
2019-05-23 15:18:50 -0300

    mongo-new

--- mongo/install.sh
+++ mongo/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 clear
 echo -e "==========\nWORKING\n=========="
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-#echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-apt-get update
-apt-get install -y mongodb-org --force-yes
 
-echo -e "==========\nINIT\n=========="
-cp ./config/mongod /etc/init.d/mongod
-update-rc.d mongod defaults
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt update
+apt install -y mongodb-org --force-yes
+systemctl start mongod.service
+systemctl enable mongod.service
+mongod --version
 
 echo -e "==========\nREADY\n=========="
 
