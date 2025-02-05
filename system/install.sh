869ba9fddfb38da9527ed2108646600e7e13f650
Gustavo D. Ripoll <gdripoll@gmail.com>
2025-02-05 15:52:37 -0300

    init2

--- system/install.sh
+++ system/install.sh
@@ -10,10 +10,9 @@ fi
 # update
 apt-get update
 
-
 echo -e "==========\nWORKING\n=========="
 # basics
-apt-get -y install git curl wget unzip htop vim mc
+apt-get -y install curl wget unzip htop nmon vim mc cifs-utils net-tools
 # clean
 apt-get autoremove
 apt-get clean
@@ -27,9 +26,11 @@ echo "syntax on" >> /home/$1/.vimrc
 echo "set nowrap" >> /home/$1/.vimrc
 chown $1:$1 /home/$1/.vimrc
 
+
 echo -e "==========\nSETTING root \n=========="
 
 echo "alias ll='ls -al'" >> /root/.bashrc
+echo "PATH=$PATH:/usr/sbin/" >> /root/.bashrc
 echo "set tabstop=2" > /root/.vimrc
 echo "set nu" >> /root/.vimrc
 echo "syntax on" >> /root/.vimrc
