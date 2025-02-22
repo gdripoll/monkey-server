9720b8de5aa9a205cf0a5693b038a2785cb09ed2
Gustavo D. Ripoll <gdripoll@gmail.com>
2025-02-22 15:55:45 -0300

    lemp

--- LEMP/install.sh
+++ LEMP/install.sh
@@ -19,8 +19,14 @@ apt-get install php-fpm php-mysql
 echo "--config php---------------------------"
 echo "poner  cgi.fix_pathinfo=0 en el archivo"
 read
-vim /etc/php/7.4/fpm/php.ini
-systemctl restart php7.4-fpm
+vim /etc/php/8.2/fpm/php.ini
+systemctl restart php8.2-fpm
+
+apt -y install certbot python3-certbot-nginx
+certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --email gdripoll@gmail.com -d centralservices.ddns.net
+
+
+
 echo "--config nginx-------------------------"
 cp sitios_config /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/sitios_config /etc/nginx/sites-enabled/
